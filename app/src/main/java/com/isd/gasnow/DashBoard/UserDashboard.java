a09c1e3060a36368cf1cb9e35a78e6b3605746fc
Asm Nahin <asmnahin21@gmail.com>
2023-02-12 11:46:58 +0600

    Navigation Drawer UI done

--- app/src/main/java/com/isd/gasnow/DashBoard/UserDashboard.java
+++ app/src/main/java/com/isd/gasnow/DashBoard/UserDashboard.java
@@ -1,25 +1,40 @@
 package com.isd.gasnow.DashBoard;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
+import com.google.android.material.navigation.NavigationView;
 import com.isd.gasnow.DashBoard.FeaturedAdapter;
 import com.isd.gasnow.DashBoard.FeaturedHelperClass;
 import com.isd.gasnow.R;
 
 import java.util.ArrayList;
 
-public class UserDashboard extends AppCompatActivity {
+public class UserDashboard extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    RecyclerView featureRecycler,mostViewedRecycler, categoriesRecycler;
+    RecyclerView featureRecycler, mostViewedRecycler, categoriesRecycler;
     RecyclerView.Adapter adapter;
 
-    private GradientDrawable gradient1, gradient2, gradient3, gradient4;
+    static final float END_SCALE = 0.7f;
+
+//    private GradientDrawable gradient1, gradient2, gradient3, gradient4;
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    ImageView menuIcon;
+    LinearLayout contentView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,13 +44,76 @@ public class UserDashboard extends AppCompatActivity {
         featureRecycler = findViewById(R.id.dashboardFeaturedRecycleView);
         mostViewedRecycler = findViewById(R.id.dashboardMostviewedRecyclerView);
         categoriesRecycler = findViewById(R.id.dashboardCategoriesRecyclerView);
+        drawerLayout = findViewById(R.id.nav_drawer_layout);
+        navigationView = findViewById(R.id.nav_navigationview);
+        menuIcon = findViewById(R.id.navbar_menuIcon);
+        contentView = findViewById(R.id.content);
 
 
-        
         featuredRecycler();
         mostViewedRecycler();
         categoriesRecycler();
+        navigataionDrawer();
 
+    }
+
+    private void navigataionDrawer() {
+        navigationView.bringToFront();
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_home);
+
+        menuIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (drawerLayout.isDrawerVisible(GravityCompat.START))
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                else
+                    drawerLayout.openDrawer(GravityCompat.START);
+
+            }
+        });
+
+        animateNavigationDrawer();
+    }
+
+    private void animateNavigationDrawer() {
+
+        drawerLayout.setScrimColor(getResources().getColor(R.color.sweet_pink));
+        drawerLayout.addDrawerListener(new DrawerLayout.SimpleDrawerListener() {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                final float diffScaledOffset = slideOffset * (1-END_SCALE);
+                final float offsetScale = 1 - diffScaledOffset;
+                contentView.setScaleX(offsetScale);
+                contentView.setScaleY(offsetScale);
+
+                final float xOffset = drawerView.getWidth() * slideOffset;
+                final float xOffsetDiff = contentView.getWidth() * diffScaledOffset;
+                final float xTranslation = xOffset - xOffsetDiff;
+                contentView.setTranslationX(xTranslation);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        });
+
+
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return true;
+    }
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerVisible(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
     }
 
     private void categoriesRecycler() {
@@ -56,14 +134,13 @@ public class UserDashboard extends AppCompatActivity {
         categoriesRecycler.setAdapter(adapter);
     }
 
-
     private void mostViewedRecycler() {
         mostViewedRecycler.setHasFixedSize(true);
-        mostViewedRecycler.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+        mostViewedRecycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
 
         ArrayList<MostViewedAdapterHelperClass> mostViewedAdapterHelperClassArrayList = new ArrayList<>();
-        mostViewedAdapterHelperClassArrayList.add(new MostViewedAdapterHelperClass(R.drawable.card1,"Gas", "Lorem ipsum abc abc sjdsjddskd"));
-        mostViewedAdapterHelperClassArrayList.add(new MostViewedAdapterHelperClass(R.drawable.card2,"Store", "Lorem ipsum abc abc sjdsjddskd"));
+        mostViewedAdapterHelperClassArrayList.add(new MostViewedAdapterHelperClass(R.drawable.card1, "Gas", "Lorem ipsum abc abc sjdsjddskd"));
+        mostViewedAdapterHelperClassArrayList.add(new MostViewedAdapterHelperClass(R.drawable.card2, "Store", "Lorem ipsum abc abc sjdsjddskd"));
 
         adapter = new MostViewedAdapter(mostViewedAdapterHelperClassArrayList);
         mostViewedRecycler.setAdapter(adapter);
@@ -71,15 +148,18 @@ public class UserDashboard extends AppCompatActivity {
 
     private void featuredRecycler() {
         featureRecycler.setHasFixedSize(true);
-        featureRecycler.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+        featureRecycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
 
         ArrayList<FeaturedHelperClass> featuredHelperClassArrayList = new ArrayList<>();
-        featuredHelperClassArrayList.add(new FeaturedHelperClass(R.drawable.card1,"Gas", "Lorem ipsum"));
-        featuredHelperClassArrayList.add(new FeaturedHelperClass(R.drawable.card2,"Store", "Lorem ipsum"));
+        featuredHelperClassArrayList.add(new FeaturedHelperClass(R.drawable.card1, "Gas", "Lorem ipsum"));
+        featuredHelperClassArrayList.add(new FeaturedHelperClass(R.drawable.card2, "Store", "Lorem ipsum"));
 
         adapter = new FeaturedAdapter(featuredHelperClassArrayList);
         featureRecycler.setAdapter(adapter);
 
 
     }
+
+
+
 }
